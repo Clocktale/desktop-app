b5058769b8798954b0ff38a84d1c0dee327489be
EdineiLuizPereiraJr <edineijr2005@gmail.com>
2025-08-02 20:53:57 -0300

    feat(styles):update

--- src/main/java/constants/ColorConstants.java
+++ src/main/java/constants/ColorConstants.java
@@ -2,14 +2,21 @@ package constants;
 
 public class ColorConstants {
     //Paleta base
-    public static final String PRIMARY = "#D4AF37"; 
+    public static final String PRIMARY = "#D4AF37";
     public static final String SECONDARY = "#757575"; 
     public static final String BACKGROUND = "#0B1C2C"; 
     public static final String TEXT_DARK = "#757575";
     public static final String TEXT_LIGHT = "#FFFFFF";
 
-    // Estados
-    public static final String SUCCESS = "#0f9b14ff";
-    public static final String ERROR = "#ea0d0dff";
+    //Cores decorativas
+    public static final String D_LIGHT_PURPLE = "#BC00E6";
+    public static final String D_DARK_PURPLE = "#4C004E";
+    public static final String D_LIGHT_GREEN = "#07C600";
+    public static final String D_DARK_GREEN = "#0D6500";
+
+    //Estados
+    public static final String SUCCESS = "#07C600";
+    public static final String ERROR = "#FE2D2D";
     public static final String WARNING = "#ff7707ff";
+    
 }
--- src/main/java/constants/FontConstants.java
+++ src/main/java/constants/FontConstants.java
@@ -1,11 +1,13 @@
 package constants;
 
-public class FontConstants {
-    public static final double FONT_XXS = 10;
-    public static final double FONT_XS = 12;
-    public static final double FONT_SM = 14;
-    public static final double FONT_MD = 16;
-    public static final double FONT_LG = 20;
-    public static final double FONT_XL = 24;
-    public static final double FONT_XXL = 32;
+public class FontConstants {     
+    public static final double FONT_SM = 16;     
+    public static final double FONT_M = 18;      
+    public static final double FONT_MD = 20;     
+    public static final double FONT_ML = 22;     
+    public static final double FONT_LG = 24;     
+    public static final double FONT_XL = 28;     
+    public static final double FONT_XXL = 36;    
+    public static final double FONT_XXXL = 48;   
+    public static final double FONT_DISPLAY = 64; 
 }
